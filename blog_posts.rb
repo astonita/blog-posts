39cfb9e8b3c334cf12cf8626455d3d396fa31dbd
Jay <jpnappy@gmail.com>
2015-09-20 16:14:25 -0400

    added a bining

--- blog_posts.rb
+++ blog_posts.rb
@@ -1,5 +1,7 @@
 require 'pry'
 
+
+
 blog_posts = [
   {
     title: 'What Makes A Star Starry? Is It Me?',
@@ -32,3 +34,5 @@ blog_posts = [
     tags: ['history', 'morality', 'prison']
   }
 ]
+
+binding.pry
